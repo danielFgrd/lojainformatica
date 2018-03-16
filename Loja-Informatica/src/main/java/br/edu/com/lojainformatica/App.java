5ae1110db06ed9a7fbe53df8147f8f110f03be27
DanielFgrd <daniel.figueiredo01@etec.sp.gov.br>
2018-03-16 17:30:25 -0300

    fim das telas

---
+++ Loja-Informatica/src/main/java/br/edu/com/lojainformatica/App.java
@@ -0,0 +1,112 @@
+package br.edu.com.lojainformatica;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class App extends JFrame {
+	
+	TelaCadastro telaCadClientes = new TelaCadClientes();
+	TelaCadastro telaCadHardware = new TelaCadHardware();
+	TelaCadastro telaCadVendas = new TelaCadVendas();
+	
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem menuItem;
+
+	public App() {
+		this.setVisible(true);
+		this.setSize(800, 600);
+		// https://docs.oracle.com/javase/tutorial/uiswing/layout/visual.html#border
+		this.setLayout(new BorderLayout());
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		configuraMenu();
+		this.pack();
+	}
+	
+	
+	private void configuraMenu() {
+		this.menuBar = new JMenuBar();
+		
+		JMenu menuCadastro = new JMenu("CADASTRO");
+		
+		JMenuItem menuItemClientes = new JMenuItem("Clientes");
+		
+		menuItemClientes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("menuClieActionPerformed");
+				
+				App.this.telaCadHardware.setVisible(false);
+				App.this.telaCadClientes.setVisible(true);
+				App.this.telaCadVendas.setVisible(false);
+				
+				App.this.getContentPane().add(App.this.telaCadClientes, BorderLayout.CENTER);
+				
+				App.this.pack();
+			}
+						
+		});
+		
+		JMenuItem menuItemHardware = new JMenuItem("Hardware");//cria um botão no menu
+		
+		menuItemHardware.addActionListener(new ActionListener() {//inicia um evento nesse botão
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("menuHardwareActionPerformed");
+				
+				App.this.telaCadClientes.setVisible(false);
+				App.this.telaCadHardware.setVisible(true);
+				App.this.telaCadVendas.setVisible(false);
+				
+				App.this.getContentPane().add(telaCadHardware, BorderLayout.CENTER);
+				
+				App.this.pack();
+				
+			}
+		});
+		
+		JMenuItem menuItemVendas = new JMenuItem("Vendas");
+		
+		menuItemVendas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+			System.out.println("menuItemVendasActionPerformed");
+			
+			App.this.telaCadClientes.setVisible(false);
+			App.this.telaCadHardware.setVisible(false);
+			App.this.telaCadVendas.setVisible(true);
+			
+			App.this.getContentPane().add(telaCadVendas, BorderLayout.CENTER);
+		
+			App.this.pack();
+			}
+		});
+		
+		menuCadastro.add(menuItemHardware);
+		menuCadastro.add(menuItemClientes);
+		menuCadastro.add(menuItemVendas);
+		
+		JMenu menuCli = new JMenu("Clientes");
+		JMenu menuHard = new JMenu("Hardware");
+		JMenu menuVend = new JMenu("Vendas");
+		
+		
+		this.menuBar.add(menuCadastro);
+		
+		
+		
+		this.getContentPane().add(menuBar, BorderLayout.NORTH);
+		this.pack();
+		//adicionando mains telas ao App.java(principal)
+	}
+	
+	
+	public static void main(String[] args) {
+		App app = new App();
+	}
+
+}
---
+++ Loja-Informatica/src/main/java/br/edu/com/lojainformatica/TelaCadastro.java
@@ -0,0 +1,58 @@
+package br.edu.com.lojainformatica;
+
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JList;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+public abstract class TelaCadastro extends JPanel{
+	
+	JPanel painelDeBotoes = new JPanel();
+	JPanel painelDeCampos = new JPanel();
+	JPanel painelDeListagem = new JPanel();
+	JButton btnSalvar = new JButton("Salvar");
+	JButton btnLimpar = new JButton("Limpar");
+	JButton btnCancelar = new JButton("Cancelar");
+	JButton btnListar = new JButton("Listar");
+	JButton btnAlterar = new JButton("Alterar");
+	JTextField txtId = new JTextField("Digite o Id pra alterar");
+	JList <String>list;
+	
+
+	public TelaCadastro(int nLinhas, int nColunas) {
+		
+		//https://docs.oracle.com/javase/tutorial/uiswing/layout/layoutlist.html
+		//https://docs.oracle.com/javase/tutorial/uiswing/layout/border.html
+		BorderLayout borderLayout = new BorderLayout();
+		this.setLayout(borderLayout);
+		
+		//PAINEL PARA CAMPOS DO FORMULARIO
+		//https://docs.oracle.com/javase/tutorial/uiswing/layout/grid.html
+		GridLayout gridLayoutParaCampos = new GridLayout(nLinhas, nColunas);
+		painelDeCampos.setLayout(gridLayoutParaCampos);
+		this.add(painelDeCampos, BorderLayout.CENTER);
+		
+		//PAINEL DE BOTOES
+		this.painelDeBotoes = new JPanel();
+		this.btnLimpar = new JButton("Limpar");
+		this.btnCancelar = new JButton("Cancelar");
+		this.btnSalvar = new JButton("Salvar");
+		this.painelDeBotoes.add(btnAlterar);
+		this.painelDeBotoes.add(btnCancelar);
+		this.painelDeBotoes.add(btnLimpar);
+		this.painelDeBotoes.add(btnListar);
+		this.painelDeBotoes.add(btnSalvar);
+		this.painelDeBotoes.add(txtId);
+		
+		this.add(painelDeBotoes,BorderLayout.SOUTH);
+		
+		//PAINEL PARA LISTAGEM
+		list = new JList <String>();
+		this.painelDeListagem.add(list);
+		this.add(painelDeListagem,BorderLayout.EAST);
+		
+	}
+}
