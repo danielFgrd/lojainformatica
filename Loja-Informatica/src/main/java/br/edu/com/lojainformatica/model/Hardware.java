1f2af0ed41e9740d4db98467de355f3b99429453
DanielFgrd <daniel.figueiredo01@etec.sp.gov.br>
2018-05-09 20:45:05 -0300

    hibernate atualizado

--- Loja-Informatica/src/main/java/br/edu/com/lojainformatica/model/Hardware.java
+++ Loja-Informatica/src/main/java/br/edu/com/lojainformatica/model/Hardware.java
@@ -1,5 +1,8 @@
 package br.edu.com.lojainformatica.model;
+import javax.persistence.*;
 
+@Entity
+@Table(name="tbHardware")
 public class Hardware {
 	private Integer id_Hardware;
 	private String descricao;
@@ -7,30 +10,38 @@ public class Hardware {
 	private int quant_atual;
 	private int quant_minima;
 	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY) 
+	@Column (name="idHardware")
 	public Integer getId_Hardware() {
 		return id_Hardware;
 	}
 	public void setId_Hardware(Integer id_Hardware) {
 		this.id_Hardware = id_Hardware;
 	}
+	
+	@Column (name="harDesc")
 	public String getDescricao() {
 		return descricao;
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	@Column (name="harPreco")
 	public double getPreco_unit() {
 		return preco_unit;
 	}
 	public void setPreco_unit(double preco_unit) {
 		this.preco_unit = preco_unit;
 	}
+	
+	@Column (name="harQuantAtual")
 	public int getQuant_atual() {
 		return quant_atual;
 	}
 	public void setQuant_atual(int quant_atual) {
 		this.quant_atual = quant_atual;
 	}
+	@Column (name="harQuantMinima")
 	public int getQuant_minima() {
 		return quant_minima;
 	}
--- Loja-Informatica/src/main/java/br/edu/com/lojainformatica/model/Vendas.java
+++ Loja-Informatica/src/main/java/br/edu/com/lojainformatica/model/Vendas.java
@@ -1,27 +1,42 @@
 package br.edu.com.lojainformatica.model;
 import java.util.Date;
+import javax.persistence.*;
 
+@Entity
+@Table (name="tbVenda")
 public class Vendas {
 	private Integer id_venda;
+	private Integer id_hardware;
 	private Integer id_clientes;
 	private Date data;
 	private Double vlr_total;
 	private Double desconto;
 	private Double vrl_pago;
 	
-	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	public Integer getId_venda() {
 		return id_venda;
 	}
 	public void setId_venda(Integer id_venda) {
 		this.id_venda = id_venda;
 	}
+	
+	@Id
 	public Integer getId_clientes() {
 		return id_clientes;
 	}
 	public void setId_clientes(Integer id_clientes) {
 		this.id_clientes = id_clientes;
 	}
+	
+	@Id
+	public Integer getId_hardware() {
+		return id_hardware;
+	}
+	public void setId_hardware(Integer id_hardware) {
+		this.id_hardware = id_hardware;
+	}
+	
 	public Date getData() {
 		return data;
 	}
